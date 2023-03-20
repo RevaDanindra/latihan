0451b3cc4976fec01581bbeb6daf39db001a4f8a
RevaDanindra <reva42@students.amikom.ac.id>
2023-03-20 21:52:35 +0700

    latihan taxi

---
+++ Taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Taxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("OnDuty : Yes");
+            }
+            if (OnDuty == false)
+            { 
+                Console.WriteLine("OnDuty : No");
+            }
+            Console.WriteLine("Number Of Passenger : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        { 
+            Console.WriteLine("\nReva sedang menjemput penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("Reva selesai mengantar penumpang");
+        }
+    }
+}
